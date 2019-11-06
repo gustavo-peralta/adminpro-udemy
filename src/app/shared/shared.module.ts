2f25ccde2c5c250e29e0217bbfd1edf089c16d66
Gustavo <guper08@gmail.com>
2019-11-06 20:00:22 +0100

    Reestructuracion de los modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { PagesModule } from '../pages/pages.module';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ]
+})
+export class SharedModule { }
